d7abdabf10b4ff3da49d888bc5f7de8bf99b7365
VitorBertoldi <117694686+VitorBertoldi@users.noreply.github.com>
2024-10-20 18:22:59 -0300

    db config

--- config/database.js
+++ config/database.js
@@ -1,8 +1,22 @@
 const { Sequelize } = require('sequelize');
+require('dotenv').config(); 
 
-const sequelize = new Sequelize('faturas', 'postgres', 'v96160770', {
-    host: 'localhost',          
+const sequelize = new Sequelize(process.env.POSTGRES_URL, {
     dialect: 'postgres',
+    dialectOptions: {
+        ssl: {
+            require: true, 
+            rejectUnauthorized: false 
+        }
+    },
 });
+
+sequelize.authenticate()
+    .then(() => {
+        console.log('Connection has been established successfully.');
+    })
+    .catch(err => {
+        console.error('Unable to connect to the database:', err);
+    });
 
 module.exports = sequelize;
